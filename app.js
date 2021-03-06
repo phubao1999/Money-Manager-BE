bac72272659ade12e3310be4f03d6ba86f79e06c
BaoTuong <baopt@vnext.com.vn>
2021-03-07 03:41:10 +0700

    Update P2"

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const stringConnection = process.env.DB_CONNECTION.replace('<NAME>', process.env
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(stringConnection)
-    .then(() => console.log('Connect DB Successfully. And This is test git fetch'))
+    .then(() => console.log('Connect DB Successfully. And This is test git fetch. Test Source Tree 2'))
     .catch(err => console.log(err))
 
 app.use(cors(corsMidlleWare));
