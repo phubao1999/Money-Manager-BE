6107a4088d62e2c6dac053ea2babaf2216196e48
BaoTuong <baopt@vnext.com.vn>
2021-03-23 23:29:42 +0700

    Update Env Variables

--- util/jwtUtil.js
+++ util/jwtUtil.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+require('dotenv/config');
 
 const generateAccessToken = user => {
-    return jwt.sign(user, 'tokenSecret', { expiresIn: '1800s' });
+    return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
 }
 
 module.exports = { generateAccessToken };
