653a11e50765e6d5d8f9464695782abb551119f4
BaoTuong <baopt@vnext.com.vn>
2021-04-15 23:20:06 +0700

    Fix routing

--- src/routes/routing.ts
+++ src/routes/routing.ts
@@ -1,10 +1,11 @@
-import { Router } from 'express';
-const router = Router();
-import auth from './auth/auth';
+import * as express from "express";
+import authRouter from './auth/auth';
 import post from './post/post';
 import verifiTokenMiddleWares from '../middlewares/verifyToken.middlewares';
 
-export default function routing() {
-    router.use('/auth', auth);
-    router.use('/post', verifiTokenMiddleWares, post);
-}
+let router = express.Router();
+
+router.use('/auth', authRouter);
+router.use('/post', verifiTokenMiddleWares, post);
+
+export default router;
