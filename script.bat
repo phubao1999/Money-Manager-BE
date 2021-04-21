0af76c4df17f99e9d465a9a0531f6e155aeb977f
BaoTuong <baopt@vnext.com.vn>
2021-04-21 23:19:48 +0700

    Update Batch File

--- script.bat
+++ script.bat
@@ -1,6 +1,6 @@
-SET dbConnection=--build-arg db_connection=mongodb+srv://"<"NAME">":"<"PASSWORD">""@"money-manager.1hv81.mongodb.net/demo?retryWrites=true"&"w=majority
-SET name=--build-arg NAME=phutuongbao1999
-SET password=--build-arg PASSWORD=baodhqb05211
+SET dbConnection=--build-arg db_connection=db_connection
+SET name=--build-arg NAME=NAME
+SET password=--build-arg PASSWORD=PASSWORD
 
 docker build -t baopt-my-node-app . %dbConnection% %name% %password%
 
