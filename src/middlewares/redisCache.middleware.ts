d1fec7678bf819959c9cdf8b59205e9f24e435b3
BaoTuong <baopt@vnext.com.vn>
2021-05-13 22:24:29 +0700

    Update redis (Install redis local have been failed)

--- src/middlewares/redisCache.middleware.ts
+++ src/middlewares/redisCache.middleware.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
 import * as env from "../environments/enviroments";
+import ResponseHelper from "../helper/responseHelper";
 
 const redis = require("redis");
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PASSWORD || "",
+  password: process.env.REDIS_PORT || 6379,
+});
 
 export default {
   cacheData(key: any, value: any) {
-    return client.settex(key, env.environment.redis_cache, value);
+    return client.set(key, env.environment.redis_cache, value.toString());
+  },
+  getCacheData(key: string) {
+    return client.get(key, (err: Error, reply: any) => {
+      if (err) throw err;
+      console.log(reply);
+      return reply;
+    });
   },
   cacheMiddleware(req: Request, res: Response, next: any) {
     const { dataKey } = req.params;
@@ -16,9 +27,7 @@ export default {
       if (err) throw err;
 
       if (data !== null) {
-        res.send({
-          dataKey: data,
-        });
+        ResponseHelper.sendResponse(data, "Data Process Success");
       } else {
         next();
       }
